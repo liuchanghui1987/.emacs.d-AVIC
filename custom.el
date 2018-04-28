f79e4cf3864697dca585b84c1f2e2aa5ee0a8f38
Your Name <liuchanghui1987@gmail.com>
2018-04-28 20:41:49 +0800

    update

--- custom.el
+++ custom.el
@@ -9,11 +9,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
  '(custom-enabled-themes (quote (sanityinc-tomorrow-bright)))
  '(custom-safe-themes
    (quote
-    ("1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+    ("191a1493fc7c3252ae949cc42cecc454900e3d4d1feb96f480cf9d1c40c093ee" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
  '(package-archives
    (quote
     (("org" . "http://orgmode.org/elpa/")
